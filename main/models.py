1c84a89bb787b1dc9244d43e08b99987d1d4b883
Connor-Greg <connor.gregorius@gmail.com>
2025-11-13 14:04:23 -0600

    More stuff

--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class WishlistItem(models.Model):
+    participant = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='wishlist_items'
+    )
+    item_name = models.CharField(max_length=200)
+    details = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.item_name} ({self.participant.name})"
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User, WishlistItem
+
 
 def index(request):
     return render(request, 'index.html')
@@ -7,7 +9,31 @@ def homepage(request):
     return render(request, 'homepage.html')
 
 def wish(request):
-    return render(request, 'wish.html')
+    participants = User.objects.all()
+
+    if request.method == 'POST':
+        participant_id = request.POST.get('personSelect')
+        item_name = request.POST.get('itemName')
+        details = request.POST.get('details')
+
+        participant = User.objects.get(id=participant_id)
+
+        # Save single item
+        WishlistItem.objects.create(
+            participant=participant,
+            item_name=item_name,
+            details=details
+        )
+
+        return redirect('wish')
+
+    return render(request, 'wish.html', {'participants': participants})
 
 def yourperson(request):
-    return render(request, 'yourperson.html')
+    # THIS WHOLE THING NEEDS TO BE CHANGED ONCE EVERYONE HAS FILLED IN THEIR WISHLIST
+    participant = User.objects.first()
+
+    return render(request, 'yourperson.html', {
+        'participant': participant,
+        'wishlist_items': participant.wishlist_items.all()
+    })
